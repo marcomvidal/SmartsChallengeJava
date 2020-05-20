a6311564816f71948df8d5dadf616406ad2917a6
Marco M. Vidal <marcomvidal@gmail.com>
2020-05-20 14:42:22 -0300

    Back-end CORS adjustments

--- dashboard-incorporadora-backend/src/main/java/br/com/marcomvidal/dashboardincorporadorabackend/DependencyContainer.java
+++ dashboard-incorporadora-backend/src/main/java/br/com/marcomvidal/dashboardincorporadorabackend/DependencyContainer.java
@@ -4,6 +4,9 @@ import com.google.gson.Gson;
 import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class DependencyContainer {
